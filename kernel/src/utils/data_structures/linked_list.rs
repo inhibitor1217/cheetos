b2c3f7658d77b7dbff44dc6e301b793db6150af5
inhibitor1217 <inhibitor@kaist.ac.kr>
2023-01-29 11:14:28 +0900

    feat(kernel): add temporary implementation of linked list

--- kernel/src/utils/data_structures/linked_list.rs
+++ kernel/src/utils/data_structures/linked_list.rs
@@ -38,65 +38,92 @@
 /// ```
 #[derive(Debug)]
 pub struct LinkedList<T> {
+    front: Node,
+    back: Node,
     _marker: core::marker::PhantomData<T>,
 }
 
 impl<T> LinkedList<T> {
-    /// Creates a new, empty [`LinkedList`].
+    /// Creates a new, uninitialized [`LinkedList`].
+    #[must_use = "initializing a `LinkedList` does nothing without `.init()`"]
     pub const fn new() -> Self {
         Self {
+            front: Node::new(),
+            back: Node::new(),
             _marker: core::marker::PhantomData,
         }
     }
 
+    /// Initializes the [`LinkedList`].
+    pub fn init(&mut self) {
+        self.front.next = Some(&mut self.back);
+        self.back.prev = Some(&mut self.front);
+    }
+
     /// Provides a forward iterator.
-    pub fn iter(&self) -> Iter<T> {
+    pub fn iter(&mut self) -> Iter {
         todo!()
     }
 
     /// Returns `true` if the [`LinkedList`] is empty.
-    pub fn is_empty(&self) -> bool {
-        todo!()
+    pub fn is_empty(&mut self) -> bool {
+        self.front().is_none()
     }
 
     /// Returns the number of [`Node`]s in the [`LinkedList`].
     /// Runs in O(n) in the number of [`Node`]s.
-    pub fn len(&self) -> usize {
+    pub fn len(&mut self) -> usize {
         todo!()
     }
 
-    /// Returns a reference to the first [`Node`] of the [`LinkedList`], or
+    /// Returns `&mut` to the first [`Node`] of the [`LinkedList`], or
     /// `None` if it is empty.
-    pub fn front(&self) -> Option<&'static mut Node<T>> {
-        todo!()
+    pub fn front(&mut self) -> Option<&'static mut Node> {
+        let first = self
+            .front
+            .next
+            .expect("front node should always have a next node");
+        if first == &mut self.back {
+            None
+        } else {
+            Some(unsafe { &mut *first })
+        }
     }
 
-    /// Returns a reference to the last [`Node`] of the [`LinkedList`], or
+    /// Returns `&mut` to the last [`Node`] of the [`LinkedList`], or
     /// `None` if it is empty.
-    pub fn back(&self) -> Option<&'static mut Node<T>> {
-        todo!()
+    pub fn back(&mut self) -> Option<&'static mut Node> {
+        let last = self
+            .back
+            .prev
+            .expect("back node should always have a previous node");
+        if last == &mut self.front {
+            None
+        } else {
+            Some(unsafe { &mut *last })
+        }
     }
 
     /// Inserts a [`Node`] at the front of the [`LinkedList`].
-    pub fn push_front(&mut self, _node: &'static mut Node<T>) {
-        todo!()
+    pub fn push_front(&mut self, node: &'static mut Node) {
+        self.front.insert(node);
     }
 
     /// Inserts a [`Node`] at the back of the [`LinkedList`].
-    pub fn push_back(&mut self, _node: &'static mut Node<T>) {
-        todo!()
+    pub fn push_back(&mut self, node: &'static mut Node) {
+        self.back.insert_before(node);
     }
 
     /// Removes the first [`Node`] from the [`LinkedList`] and returns it, or
     /// `None` if it is empty.
-    pub fn pop_front(&mut self) -> Option<&'static mut Node<T>> {
-        todo!()
+    pub fn pop_front(&mut self) -> Option<&'static mut Node> {
+        self.front().map(Node::remove)
     }
 
     /// Removes the last [`Node`] from the [`LinkedList`] and returns it, or
     /// `None` if it is empty.
-    pub fn pop_back(&mut self) -> Option<&'static mut Node<T>> {
-        todo!()
+    pub fn pop_back(&mut self) -> Option<&'static mut Node> {
+        self.back().map(Node::remove)
     }
 }
 
@@ -110,7 +137,7 @@ where
     }
 
     /// Inserts `node` in the proper position in `self` to maintain ordering.
-    pub fn insert_ordered(&mut self, _node: &'static mut Node<T>) {
+    pub fn insert_ordered(&mut self, _node: &'static mut Node) {
         todo!()
     }
 
@@ -129,60 +156,100 @@ where
 }
 
 impl<T> core::iter::IntoIterator for LinkedList<T> {
-    type Item = Node<T>;
-    type IntoIter = Iter<T>;
+    type Item = Node;
+    type IntoIter = Iter;
 
-    fn into_iter(self) -> Self::IntoIter {
+    fn into_iter(mut self) -> Self::IntoIter {
         self.iter()
     }
 }
 
 #[derive(Debug)]
-pub struct Node<T> {
-    _marker: core::marker::PhantomData<T>,
+pub struct Node {
+    prev: Option<*mut Node>,
+    next: Option<*mut Node>,
 }
 
-impl<T> Node<T> {
+impl Node {
     /// Creates a new [`Node`].
-    pub fn new() -> Self {
+    pub const fn new() -> Self {
         Self {
-            _marker: core::marker::PhantomData,
+            prev: None,
+            next: None,
         }
     }
 
     /// Inserts `node` after `self`.
-    pub fn insert(&mut self, _node: &'static mut Node<T>) {
-        todo!()
+    pub fn insert(&mut self, node: &mut Node) {
+        // Cannot insert after back node.
+        assert!(!self.is_back());
+
+        node.prev = Some(self);
+        node.next = self.next;
+        unsafe {
+            (*self.next.expect("Cannot insert after back node of list")).prev = Some(node);
+        }
+        self.next = Some(node);
+    }
+
+    /// Inserts `node` before `self`.
+    pub fn insert_before(&mut self, node: &mut Node) {
+        // Cannot insert before front node.
+        assert!(!self.is_front());
+
+        node.prev = self.prev;
+        node.next = Some(self);
+        unsafe {
+            (*self.prev.expect("Cannot insert before front node of list")).next = Some(node);
+        }
+        self.prev = Some(node);
     }
 
     /// Removes elements `from` through `last` (exclusive) from their current
     /// list, then inserts them after `self`.
-    pub fn insert_slice(&mut self, _from: &'static mut Node<T>, _to: &'static mut Node<T>) {
+    pub fn insert_slice(&mut self, _from: &mut Node, _to: &mut Node) {
         todo!()
     }
 
-    /// Removes `self` from its current list.
-    pub fn remove(&mut self) {
-        todo!()
+    /// Removes `self` from its current list, then returns itself.
+    pub fn remove(&mut self) -> &mut Self {
+        // Cannot remove the front or back node.
+        assert!(self.is_interior());
+
+        unsafe {
+            (*self.prev.expect("Cannot remove front node of list")).next = self.next;
+            (*self.next.expect("Cannot remove back node of list")).prev = self.prev;
+        }
+
+        self.prev = None;
+        self.next = None;
+
+        self
+    }
+
+    /// Returns `true` if `self` is the first [`Node`] in its list.
+    fn is_front(&self) -> bool {
+        self.prev.is_none() && self.next.is_some()
+    }
+
+    /// Returns `true` if `self` is an interior [`Node`] in its list.
+    fn is_interior(&self) -> bool {
+        self.prev.is_some() && self.next.is_some()
+    }
+
+    /// Returns `true` if `self` is the last [`Node`] in its list.
+    fn is_back(&self) -> bool {
+        self.prev.is_some() && self.next.is_none()
     }
 }
 
-impl<T> core::ops::Deref for Node<T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        todo!()
-    }
-}
-
-impl<T> core::ops::DerefMut for Node<T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        todo!()
-    }
-}
-
+/// Returns `&mut T` from `&mut Node<T>`, given the name of the [`Node`]'s
+/// field of the struct `T`.
+#[macro_export]
 macro_rules! get_element {
-    () => {};
+    ($node:expr, $container:ty, $field:ident) => {
+        &mut *(($node as *mut Node<$container).offset(-crate::utils::offset_of!($container, $field)) as *mut $container)
+    };
 }
 
 /// An iterator over the elements of a [`LinkedList`].
@@ -192,12 +259,10 @@ macro_rules! get_element {
 /// meant to be statically allocated, the user should ensure the list elements
 /// are properly synchronized.
 #[derive(Debug)]
-pub struct Iter<T> {
-    _marker: core::marker::PhantomData<T>,
-}
+pub struct Iter {}
 
-impl<T> core::iter::Iterator for Iter<T> {
-    type Item = Node<T>;
+impl core::iter::Iterator for Iter {
+    type Item = Node;
 
     fn next(&mut self) -> Option<Self::Item> {
         todo!()
