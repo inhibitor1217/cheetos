53c70d8964d474f505ce4bc3eada110b19df3396
inhibitor1217 <inhibitor@kaist.ac.kr>
2023-02-05 13:51:56 +0900

    feat(kernel): implement PageAllocator::get_pages

--- kernel/src/threads/palloc.rs
+++ kernel/src/threads/palloc.rs
@@ -7,6 +7,17 @@ use super::{
     sync::lock,
 };
 
+bitflags::bitflags! {
+    /// Flags for allocating pages.
+    pub struct AllocateFlags: u8 {
+        /// Zero the page contents.
+        const ZERO = 0b0000_0001;
+
+        /// User page.
+        const USER = 0b0000_0010;
+    }
+}
+
 /// Page allocator. Hands out memory in page-size (or page-multiple) chunks.
 ///
 /// System memory is divided into two [`Pool`]s called the kernel and user
@@ -18,7 +29,7 @@ use super::{
 /// By default, half of system RAM is given to the kernel pool and half to the
 /// user pool. That should be huge overkill for the kernel pool, but that's just
 /// fine for demonstration purposes.
-struct PageAllocator {
+pub struct PageAllocator {
     /// Memory pool for kernel data.
     kernel_pool: lock::Mutex<Pool>,
 
@@ -34,7 +45,9 @@ impl PageAllocator {
         }
     }
 
-    fn init(&self, boot_info: &'static bootloader_api::BootInfo, user_page_limit: usize) {
+    /// Initializes the page allocator. At most `user_page_limit` pages are put
+    /// into the user pool.
+    pub fn init(&self, boot_info: &'static bootloader_api::BootInfo, user_page_limit: usize) {
         // Retrieve the usable memory region from bootloader metadata.
         let free_region = boot_info
             .memory_regions
@@ -60,6 +73,58 @@ impl PageAllocator {
         self.user_pool
             .lock()
             .init(user_start.as_mut_ptr(), user_pages, "user pool");
+    }
+
+    /// Obtains a single free page and returns the allocated page.
+    ///
+    /// - If `AllocateFlags::USER` is set, the page is obtained from the user
+    /// pool, otherwise from the kernel pool.
+    /// - If `AllocateFlags::ZERO` is set, the page is filled with zeros.
+    ///
+    /// If no pages are available, returns `None`.
+    pub fn get_page(&self, flags: AllocateFlags) -> Option<Page> {
+        self.get_pages(1, flags)
+    }
+
+    /// Obtains and returns a group of `count` contiguous free pages.
+    ///
+    /// - If `AllocateFlags::USER` is set, the page is obtained from the user
+    /// pool, otherwise from the kernel pool.
+    /// - If `AllocateFlags::ZERO` is set, the page is filled with zeros.
+    ///
+    /// If too few pages are available, returns `None`.
+    pub fn get_pages(&self, count: usize, flags: AllocateFlags) -> Option<Page> {
+        if count == 0 {
+            return None;
+        }
+
+        let Self {
+            kernel_pool,
+            user_pool,
+        } = self;
+
+        let pool = if flags.contains(AllocateFlags::USER) {
+            user_pool
+        } else {
+            kernel_pool
+        };
+
+        let pages_start = {
+            let pool = pool.lock();
+            let used_map = unsafe { &mut (*pool.used_map.unwrap().as_ptr()) };
+            if let Some(page_index) = used_map.scan(0, count, false) {
+                used_map.set_many(page_index, count, true);
+                Some(pool.base.unwrap() + page_index as u64)
+            } else {
+                None
+            }
+        };
+
+        if flags.contains(AllocateFlags::ZERO) {
+            // TODO: memset(pages_start.start_address(), 0, count * PAGE_SIZE);
+        }
+
+        pages_start
     }
 }
 
@@ -103,10 +168,9 @@ impl Pool {
 }
 
 /// A global page allocator.
-static PAGE_ALLOCATOR: PageAllocator = PageAllocator::new();
+pub static PAGE_ALLOCATOR: PageAllocator = PageAllocator::new();
 
-/// Initializes the page allocator. At most `user_page_limit` pages are put
-/// into the user pool.
+/// Initialize the page allocation from the physical memory.
 pub fn init(boot_info: &'static bootloader_api::BootInfo, user_page_limit: usize) {
     PAGE_ALLOCATOR.init(boot_info, user_page_limit);
 }
