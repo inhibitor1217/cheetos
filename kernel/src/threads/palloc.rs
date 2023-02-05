a97ec335ee4d576db179fb712756bf663444b726
inhibitor1217 <inhibitor@kaist.ac.kr>
2023-02-05 12:34:31 +0900

    feat(kernel): initialize the page allocation pool

--- kernel/src/threads/palloc.rs
+++ kernel/src/threads/palloc.rs
@@ -1,6 +1,11 @@
-use crate::println;
+use core::ptr;
 
-use super::sync::lock;
+use crate::{div_round_up, println, utils::data_structures::bit_set::BitSet};
+
+use super::{
+    addr::{ptov, Page, PhysAddr, VirtAddr, PAGE_SIZE},
+    sync::lock,
+};
 
 /// Page allocator. Hands out memory in page-size (or page-multiple) chunks.
 ///
@@ -21,9 +26,6 @@ struct PageAllocator {
     user_pool: lock::Mutex<Pool>,
 }
 
-type VirtAddr = x86_64::VirtAddr;
-type Page = x86_64::structures::paging::Page<x86_64::structures::paging::Size4KiB>;
-
 impl PageAllocator {
     const fn new() -> Self {
         Self {
@@ -39,29 +41,64 @@ impl PageAllocator {
             .iter()
             .find(|region| region.kind == bootloader_api::info::MemoryRegionKind::Usable)
             .unwrap();
-        let free_start = Page::containing_address(VirtAddr::new(free_region.start));
-        let free_end = Page::containing_address(VirtAddr::new(free_region.end));
+        let free_start = ptov(PhysAddr::new(free_region.start));
+        let free_end = ptov(PhysAddr::new(free_region.end));
+        let free_start_page = Page::from_start_address(free_start).unwrap();
+        let free_end_page = Page::from_start_address(free_end).unwrap();
 
-        let free_pages = (free_end - free_start) as usize;
+        let free_pages = (free_end_page - free_start_page) as usize;
         let user_pages = user_page_limit.min(free_pages / 2);
         let kernel_pages = free_pages - user_pages;
 
+        let kernel_start = free_start;
+        let user_start = (free_start_page + kernel_pages as u64).start_address();
+
         // Give half of memory to kernel, half to user.
-        self.kernel_pool.lock().init(kernel_pages, "kernel pool");
-        self.user_pool.lock().init(user_pages, "user pool");
+        self.kernel_pool
+            .lock()
+            .init(kernel_start.as_mut_ptr(), kernel_pages, "kernel pool");
+        self.user_pool
+            .lock()
+            .init(user_start.as_mut_ptr(), user_pages, "user pool");
     }
 }
 
 /// A memory pool.
-struct Pool {}
+struct Pool {
+    /// [`BitSet`] of free pages.
+    ///
+    /// We'll store the data structure itself at the start of the available
+    /// region. Therefore it is stored as a pointer, not a owned data structure.
+    used_map: Option<ptr::NonNull<BitSet>>,
+
+    /// Starting page of the available region in the pool.
+    base: Option<Page>,
+}
 
 impl Pool {
     const fn new() -> Self {
-        Self {}
+        Self {
+            used_map: None,
+            base: None,
+        }
     }
 
-    fn init(&mut self, page_count: usize, name: &str) {
+    fn init(&mut self, buf: *mut u8, page_count: usize, name: &str) {
+        let pages_used = div_round_up!(BitSet::buffer_size(page_count), PAGE_SIZE);
+        let bytes_used = pages_used * PAGE_SIZE;
+        let page_count = page_count - pages_used;
+
         println!("{page_count} pages available in {name}.");
+
+        unsafe {
+            // Initialize the pool.
+            self.used_map = Some(ptr::NonNull::from(BitSet::from_buffer(
+                page_count, buf, bytes_used,
+            )));
+        }
+
+        let base = VirtAddr::new(buf as u64) + bytes_used;
+        self.base = Some(Page::from_start_address(base).unwrap());
     }
 }
 
