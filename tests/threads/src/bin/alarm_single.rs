749d2fc0b4e0653a34cd9ac5abde5db0347f3333
inhibitor1217 <inhibitor@kaist.ac.kr>
2023-02-08 01:17:13 +0900

    test: apply fail! macro

--- tests/threads/src/bin/alarm_single.rs
+++ tests/threads/src/bin/alarm_single.rs
@@ -1,16 +1,19 @@
 #![no_std]
 #![no_main]
 
+static TEST_NAME: &str = "alarm_single";
+
 fn kernel_main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
     kernel::init(boot_info);
 
-    kernel::devices::shutdown::power_off()
+    kernel_test::fail!("BOO");
 }
 
 kernel::entry_point!(kernel_main);
 
 #[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    kernel::println!("{info}");
     kernel::devices::shutdown::power_off()
 }
